a54ec74f501edd1b75cdd787a0ced26255602ca1
Peter Hruska <46312949+xertep@users.noreply.github.com>
2026-04-14 15:42:13 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 playwright install chromium
+
+apt-get update && apt-get install -y \
+    libnss3 \
+    libatk1.0-0 \
+    libatk-bridge2.0-0 \
+    libcups2 \
+    libxkbcommon0 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxrandr2 \
+    libgbm1 \
+    libasound2 \
+    libpangocairo-1.0-0 \
+    libx11-xcb1
